a24077b0a521b00e0b46ac437bbc62c534d61524
🦁 <duckie@ducs-MacBook-Pro.local>
2023-07-07 23:39:42 +1000

    finalized

---
+++ playback.js
@@ -0,0 +1,173 @@
+const prevButton = document.getElementById("prev");
+const nextButton = document.getElementById("next");
+const pauseButton = document.getElementById("pause");
+const playButton = document.getElementById("play");
+const audio = document.getElementById("audio");
+const maxDuration = document.getElementById("max-duration");
+const currentTimeRef = document.getElementById("current-time");
+const progressBar = document.getElementById("progress-bar");
+const currentProgress = document.getElementById("current-progress");
+const songName = document.getElementById("song-name");
+
+let loop = true;
+let index = 0;
+
+const songsList = [
+  {
+    name: "Winter Podcast 1 - Vietnamese",
+    link: "winterpodcast_vie.mp3",
+  },
+  {
+    name: "Winter Podcast 1.1 - German",
+    link: "winterpodcast_ger1.mp3",
+  },
+  {
+    name: "Winter Podcast 1.2 - German",
+    link: "winterpodcast_ger2.mp3",
+  },
+];
+
+//events object
+let events = {
+  mouse: {
+    click: "click",
+  },
+  touch: {
+    click: "touchstart",
+  },
+};
+
+let deviceType = "";
+
+//Detect touch device
+
+const isTouchDevice = () => {
+  try {
+    //We try to create TouchEvent(it would fail for desktops and throw error)
+    document.createEvent("TouchEvent");
+    deviceType = "touch";
+    return true;
+  } catch (e) {
+    deviceType = "mouse";
+    return false;
+  }
+};
+
+//Format time (convert ms to seconds, minutes and add 0 id less than 10)
+const timeFormatter = (timeInput) => {
+  let minute = Math.floor(timeInput / 60);
+  minute = minute < 10 ? "0" + minute : minute;
+  let second = Math.floor(timeInput % 60);
+  second = second < 10 ? "0" + second : second;
+  return `${minute}:${second}`;
+};
+
+const setSong = (arrayIndex) => {
+  //this extracts all the variables from the object
+  let { name, link } = songsList[arrayIndex];
+  audio.src = link;
+  songName.innerHTML = name;
+  // songImage.src = image;
+  //display duration when metadata loads
+  audio.onloadedmetadata = () => {
+    maxDuration.innerText = timeFormatter(audio.duration);
+  };
+};
+
+const playAudio = () => {
+  audio.play();
+  pauseButton.classList.remove("hide");
+  playButton.classList.add("hide");
+};
+
+//Next song
+const nextSong = () => {
+  //if loop is true then continue in normal order
+  if (loop) {
+    if (index == songsList.length - 1) {
+      //If last song is being played
+      index = 0;
+    } else {
+      index += 1;
+    }
+    setSong(index);
+
+    playAudio();
+  }
+  // } else {
+  //   //else find a random index and play that song
+  //   // let randIndex = Math.floor(Math.random() * songsList.length);
+  //   // console.log(randIndex);
+  //   // setSong(randIndex);
+  //   // playAudio();
+  // }
+};
+
+const pauseAudio = () => {
+  audio.pause();
+  pauseButton.classList.add("hide");
+  playButton.classList.remove("hide");
+};
+
+const previousSong = () => {
+  if (index > 0) {
+    pauseAudio();
+    index -= 1;
+  } else {
+    //if first song is being played
+    index = songsList.length - 1;
+  }
+  setSong(index);
+  playAudio();
+};
+
+audio.onended = () => {
+  nextSong();
+};
+
+//play button
+playButton.addEventListener("click", playAudio);
+
+//next button
+nextButton.addEventListener("click", nextSong);
+
+//pause button
+pauseButton.addEventListener("click", pauseAudio);
+
+//prev button
+prevButton.addEventListener("click", previousSong);
+
+//if user clicks on progress bar
+isTouchDevice();
+progressBar.addEventListener(events[deviceType].click, (event) => {
+  //start of progressBar
+  let coordStart = progressBar.getBoundingClientRect().left;
+  //mouse click position
+  let coordEnd = !isTouchDevice() ? event.clientX : event.touches[0].clientX;
+  let progress = (coordEnd - coordStart) / progressBar.offsetWidth;
+
+  //set width to progress
+  currentProgress.style.width = progress * 100 + "%";
+
+  //set time
+  audio.currentTime = progress * audio.duration;
+
+  //play
+  audio.play();
+  pauseButton.classList.remove("hide");
+  playButton.classList.add("hide");
+});
+
+//update progress every second
+setInterval(() => {
+  currentTimeRef.innerHTML = timeFormatter(audio.currentTime);
+  currentProgress.style.width =
+    (audio.currentTime / audio.duration.toFixed(3)) * 100 + "%";
+});
+
+window.onload = () => {
+  //initially first song
+  index = 0;
+  setSong(index);
+  //create playlist
+};
